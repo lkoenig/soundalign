bcf66e7db73c83a43c040e4b2c7e96b083096651
Lionel Koenig <lionel.koenig@gmail.com>
2016-03-04 13:24:56 +0100

    more processing

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <memory>
 #include <iostream>
 
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
 #include "soundfile.h"
 #include "feature_extractor.h"
+
 
 
 int main(int argc, char **argv)
 {
     std::cout << "Sound Alignement" << std::endl;
 
-    auto reference = std::make_shared<SoundFile>("ref.wav");
-    
+    auto reference = std::make_shared<SoundFile>("ref.wav");    
     auto ref_descriptors = std::unique_ptr<PSDFeature>(new PSDFeature(reference, 20e-3f, 10e-3f));
-    ref_descriptors->descriptors();
+    auto degraded = std::make_shared<SoundFile>("ref.wav");
+    auto deg_descriptors = std::unique_ptr<PSDFeature>(new PSDFeature(reference, 20e-3f, 10e-3f));
 
+    Eigen::MatrixXf ref_desc = ref_descriptors->descriptors();
+    Eigen::MatrixXf deg_desc = ref_descriptors->descriptors();
+    
+    Eigen::MatrixXf correlation = ref_desc.transpose() * deg_desc;
+    
+    std::cout << correlation;
     return 0;
 }
 
